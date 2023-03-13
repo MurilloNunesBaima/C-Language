f80c6db6d12596d2f36b0e9877aaf55ad681a0c8
MurilloNunesBaima <122990171+MurilloNunesBaima@users.noreply.github.com>
2023-03-12 23:36:52 -0300

    Add files via upload

---
+++ TestaComACopypastaDoBeeMovie.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    int i,j,temQuebra = 0,tamanho = 1000;
+    char* texto = malloc(sizeof(char) * tamanho);
+    char caractere;
+
+
+    printf("Escreva o texto (minusculo): ");
+    fgets(texto, tamanho, stdin);
+    fseek(stdin, 0, SEEK_END);
+    printf("Digite o caractere a ser retirado (minusculo): ");
+    scanf(" %c", &caractere);
+
+    do
+    {
+        for(i = 0;i < strlen(texto);i++)
+        {
+            if(texto[i] == '\n')
+            {
+                temQuebra = 1;
+            }
+        }
+        if(temQuebra == 0)
+        {
+            fseek(stdin, 0, SEEK_END);
+            tamanho = tamanho + 1000;
+            texto = realloc(texto, sizeof(char) * tamanho);
+            printf("Por favor, repita o texto(minusculo): ");
+            fgets(texto, tamanho, stdin);
+        }
+     } while(temQuebra != 1);
+
+
+
+     for(i = 0; i < strlen(texto); i++){
+        while(texto[i] == caractere)
+        {
+            for(j = i; j < strlen(texto); j++)
+            {
+                texto[j] = texto[j+1];
+            }
+        }
+    }
+    printf("\nTEXTO: %s", texto);
+}
